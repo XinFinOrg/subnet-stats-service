3a84b561dc69e52e2c75aed57cfc4ca43afc9b8d
Choco <trust2065@gmail.com>
2023-07-11 12:29:36 +1000

    Update block cards title

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -61,7 +61,7 @@ export default function HomePage() {
   return (
     <div className='grid gap-6 grid-col-1'>
       <Card>
-        <h1 className='pb-4 text-xl font-medium'>Subnet Blockchain</h1>
+        <h1 className='pb-4 text-xl font-medium'>subnet Blockchain</h1>
         <Blocks
           initialLastBlock={initialLastBlock}
           lastBlock={lastBlock}
@@ -72,7 +72,7 @@ export default function HomePage() {
         />
       </Card>
       <Card>
-        <h1 className='pb-4 text-xl font-medium'>Copy at the parent chain</h1>
+        <h1 className='pb-4 text-xl font-medium'>checkpoints at the parent chain</h1>
         <Blocks
           initialLastBlock={initialLastBlock}
           lastBlock={lastBlock}
