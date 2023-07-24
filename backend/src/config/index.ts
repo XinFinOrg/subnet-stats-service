85a32e0979c791fdfa5ae21a0f890cb885274021
Liam <liam.icheng.lai@gmail.com>
2023-07-24 22:37:37 +1000

    Pagination on blocks api (#20)
    * add block index in block api
    
    * remove logs
    
    * remove logs

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -2,7 +2,9 @@ import { config } from 'dotenv';
 config({ path: '.env.stats' });
 import { logger } from '../utils/logger';
 
-export const MAX_NUM_OF_BLOCKS_IN_HISTORY = 50;
+export const MAX_NUM_OF_BLOCKS_IN_HISTORY = 300;
+export const NUM_OF_BLOCKS_RETURN = 50;
+
 export const { SUBNET_URL, PARENTCHAIN_URL } = process.env;
 if (!SUBNET_URL || !PARENTCHAIN_URL) {
   logger.error('SUBNET_URL and PARENTCHAIN_URL configuration must be set!');
