c350a9ceaba05cf632a9d6b2df2e2fb1d53398fd
Choco <trust2065@gmail.com>
2023-07-28 16:20:32 +1000

    Tweak styling

--- frontend/src/components/info-list/InfoList.tsx
+++ frontend/src/components/info-list/InfoList.tsx
@@ -3,14 +3,15 @@ import Svg, { SvgNames } from '@/components/images/Svg';
 import InfoListEmpty from '@/components/info-list/components/InfoListEmpty';
 import Title from '@/components/title/Title';
 
-import type { InfoItem } from '@/types/info';
+import type { InfoListInfo } from '@/types/info';
 
 interface InfoListProps {
-  title: string;
-  info?: InfoItem;
+  title?: string;
+  noIcon?: boolean;
+  info?: InfoListInfo;
 }
 
-export default function InfoList({ title, info }: InfoListProps) {
+export default function InfoList({ title, info, noIcon }: InfoListProps) {
   if (!info) {
     return <ErrorState title={title} />;
   }
@@ -23,37 +24,40 @@ export default function InfoList({ title, info }: InfoListProps) {
 
   return (
     <>
-      <div className='flex justify-between items-center pb-6'>
-        <Title title={title} />
-        {status && (
-          <div className='inline-flex items-center'>
-            <span>Status:</span>
-            <span
-              className={`ml-1 px-3 py-2.5 bg-opacity-20 rounded-3xl font-bold leading-none
-               ${status === 'Normal' ? 'bg-sky-500 text-sky-500' : 'bg-warning text-warning'}`
-              }
-            >
-              {status}
-            </span>
-          </div>
-        )}
-      </div>
+      {title && (
+        <div className='flex justify-between items-center pb-6'>
+          <Title title={title} />
+          {status && (
+            <div className='inline-flex items-center'>
+              <span>Status:</span>
+              <span
+                className={`ml-1 px-3 py-2.5 bg-opacity-20 rounded-3xl font-bold leading-none
+                  ${status === 'Normal' ? 'bg-sky-500 text-sky-500' : 'bg-warning text-warning'}
+                `}
+              >
+                {status}
+              </span>
+            </div>
+          )}
+        </div>
+      )}
       {data.map((item, index) => {
-        return <InfoItem key={index} {...item} isFirst={index === 0} />;
+        return <InfoListInfo key={index} {...item} isFirst={index === 0} noIcon={noIcon} />;
       })}
     </>
   );
 }
 
-interface InfoItemProps extends InfoItem.Data {
+interface InfoItemProps extends InfoListInfo.Data {
+  noIcon?: boolean;
   isFirst?: boolean;
 }
 
-function InfoItem({ name, value, isFirst }: InfoItemProps) {
+function InfoListInfo({ name, value, isFirst, noIcon }: InfoItemProps) {
   return (
     <div className={`flex justify-between pb-2 border-b-2 dark:border-text-dark-400 dark:border-opacity-40 border-text-white-400 ${isFirst ? '' : 'mt-6'}`}>
       <div className='flex items-center'>
-        <Svg svgName={SvgNames.Rhombus} size='sm' />
+        {!noIcon && <Svg svgName={SvgNames.Rhombus} size='sm' />}
         <div className='pl-1.5 dark:text-text-dark-100'>{name}</div>
       </div>
       <div className='font-bold text-sm leading-[120%]'>{value}</div>
