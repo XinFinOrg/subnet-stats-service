ab841bf435dbe99cf97ec0b0724356e8063d2aa8
Jianrong <wjrjerome@gmail.com>
2023-07-29 22:35:07 +1000

    Implement the login part

--- frontend/src/services/grandmaster-manager/errors.ts
+++ frontend/src/services/grandmaster-manager/errors.ts
@@ -1,13 +1,7 @@
-
-export interface ManagerError {
-  errorType: ErrorTypes;
-  errorStatus: number;
-  errorMessage?: string;
-}
-
 export enum ErrorTypes {
   // Login section
   WALLET_NOT_INSTALLED = "WALLET_NOT_INSTALLED",
+  CONFLICT_WITH_METAMASK = "CONFLICT_WITH_METAMASK",
   WALLET_NOT_LOGIN = "WALLET_NOT_LOGIN",
   NOT_GRANDMASTER = "NOT_GRANDMASTER",
   // Transaction section
@@ -15,4 +9,13 @@ export enum ErrorTypes {
   NOT_ENOUGH_BALANCE = "NOT_ENOUGH_BALANCE",
   // Everything else
   INTERNAL_ERROR = "INTERNAL_ERROR"
+}
+
+export class ManagerError extends Error {
+  errorType: ErrorTypes;
+  
+  constructor(message?: string, errorType?: ErrorTypes) {
+    super(message);
+    this.errorType = errorType || ErrorTypes.INTERNAL_ERROR
+  }
 }
