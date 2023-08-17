bb50b0ac7f207f8c8512bffc8080932a608dc279
Choco <trust2065@gmail.com>
2023-08-17 17:22:14 +1000

    Add formatter

--- frontend/src/services/grandmaster-manager/index.ts
+++ frontend/src/services/grandmaster-manager/index.ts
@@ -1,10 +1,13 @@
-import { StatsServiceClient } from './statsServiceClient';
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import Web3, { FMT_BYTES, FMT_NUMBER } from "web3";
-import { ErrorTypes, ManagerError } from "@/services/grandmaster-manager/errors";
-import { CustomRpcMethodsPlugin } from "@/services/grandmaster-manager/extensions";
-import { ABI } from "./abi";
+import Web3, { FMT_BYTES, FMT_NUMBER } from 'web3';
+
 import { CONTRACT_ADDRESS, FIXED_CAP_VALUE } from '@/constants/config';
+import { ErrorTypes, ManagerError } from '@/services/grandmaster-manager/errors';
+import { CustomRpcMethodsPlugin } from '@/services/grandmaster-manager/extensions';
+import { weiToEther } from '@/utils/formatter';
+
+import { ABI } from './abi';
+import { StatsServiceClient } from './statsServiceClient';
 
 export interface AccountDetails {
   accountAddress: string;
@@ -18,62 +21,62 @@ export type CandidateDetailsStatus = 'MASTERNODE' | 'PROPOSED' | 'SLASHED';
 export interface CandidateDetails {
   address: string;
   delegation: number;
-  status: CandidateDetailsStatus
+  status: CandidateDetailsStatus;
 }
 
 export class GrandMasterManager {
   private initilised: boolean;
   private web3Client: Web3 | undefined;
   private web3Contract: any;
-  
+
   private rpcBasedWeb3: Web3 | undefined;
   private statsServiceClient: StatsServiceClient;
-  
+
   constructor() {
     this.initilised = false;
     if (!(window as any).ethereum) {
-      throw new ManagerError("XDC Pay Not Installed", ErrorTypes.WALLET_NOT_INSTALLED)
+      throw new ManagerError("XDC Pay Not Installed", ErrorTypes.WALLET_NOT_INSTALLED);
     }
     this.web3Client = new Web3((window as any).ethereum);
     this.web3Contract = new this.web3Client.eth.Contract(ABI, CONTRACT_ADDRESS);
     this.web3Contract.defaultHardfork = "homestead";
     this.web3Contract.defaultChain = "mainnet";
-    
+
     this.statsServiceClient = new StatsServiceClient();
   }
-  
+
   async init(forceInit = false) {
     if (this.initilised && !forceInit) {
-      return
+      return;
     }
     this.rpcBasedWeb3 = new Web3(await this.statsServiceClient.getRpcUrl());
     this.rpcBasedWeb3.registerPlugin(new CustomRpcMethodsPlugin());
     this.initilised = true;
   }
-  
+
   private async getGrandMasterAccountDetails() {
-    await this.init()
+    await this.init();
     const accounts = await this.web3Client!.eth.getAccounts();
     if (!accounts || !accounts.length || !accounts[0].length) {
       throw new Error("No wallet address found, have you logged in?");
     }
     const accountAddress = accounts[0];
-    const balance = await this.web3Client!.eth.getBalance(accountAddress, undefined, { number: FMT_NUMBER.NUMBER , bytes: FMT_BYTES.HEX });
-    const networkId = await this.web3Client!.eth.getChainId({ number: FMT_NUMBER.NUMBER , bytes: FMT_BYTES.HEX });
+    const balance = await this.web3Client!.eth.getBalance(accountAddress, undefined, { number: FMT_NUMBER.NUMBER, bytes: FMT_BYTES.HEX });
+    const networkId = await this.web3Client!.eth.getChainId({ number: FMT_NUMBER.NUMBER, bytes: FMT_BYTES.HEX });
     // TODO: Get denom, rpcAddress
     // TODO: Check with the grand master info from the node. Make sure they are the same, otherwise NOT_GRANDMASTER error
     return {
       accountAddress, balance, networkId
-    }
+    };
   }
-  
+
   /**
    * This method will detect XDC-Pay and verify if customer has alraedy loggin.
    * @returns Account details will be returned if all good
    * @throws ManagerError with type of "WALLET_NOT_INSTALLED" || "WALLET_NOT_LOGIN"
    */
   async login(): Promise<AccountDetails> {
-    await this.init()
+    await this.init();
     try {
       const { accountAddress, balance, networkId } = await this.getGrandMasterAccountDetails();
       return {
@@ -82,21 +85,21 @@ export class GrandMasterManager {
         networkId,
         denom: "hxdc",
         rpcAddress: "https://devnetstats.apothem.network/subnet"
-      }
+      };
     } catch (err: any) {
-      throw new ManagerError(err.message, ErrorTypes.WALLET_NOT_LOGIN)
+      throw new ManagerError(err.message, ErrorTypes.WALLET_NOT_LOGIN);
     }
   }
-  
+
   /**
    * Propose to add a new masternode for being a mining candidate from the next epoch
    * @param address The master node to be added
    */
   async addNewMasterNode(address: string): Promise<void> {
-    await this.init()
+    await this.init();
     try {
       const { accountAddress } = await this.getGrandMasterAccountDetails();
-      const nonce = await this.web3Client!.eth.getTransactionCount(accountAddress, undefined, { number: FMT_NUMBER.NUMBER , bytes: FMT_BYTES.HEX });
+      const nonce = await this.web3Client!.eth.getTransactionCount(accountAddress, undefined, { number: FMT_NUMBER.NUMBER, bytes: FMT_BYTES.HEX });
       await this.web3Contract.methods.propose(replaceXdcWith0x(address)).send({
         from: accountAddress,
         nonce,
@@ -105,19 +108,19 @@ export class GrandMasterManager {
         gasPrice: '250000000'
       });
     } catch (error: any) {
-      throw handleTransactionError(error)
+      throw handleTransactionError(error);
     }
   }
-    
+
   /**
    * Remove a masternode from the manager view list
    * @param address The master node to be removed
    */
   async removeMasterNode(address: string): Promise<void> {
-    await this.init()
+    await this.init();
     try {
       const { accountAddress } = await this.getGrandMasterAccountDetails();
-      const nonce = await this.web3Client!.eth.getTransactionCount(accountAddress, undefined, { number: FMT_NUMBER.NUMBER , bytes: FMT_BYTES.HEX });
+      const nonce = await this.web3Client!.eth.getTransactionCount(accountAddress, undefined, { number: FMT_NUMBER.NUMBER, bytes: FMT_BYTES.HEX });
       await this.web3Contract.methods.resign(replaceXdcWith0x(address)).send({
         from: accountAddress,
         nonce,
@@ -126,21 +129,21 @@ export class GrandMasterManager {
         gasPrice: '250000000'
       });
     } catch (error: any) {
-      throw handleTransactionError(error)
+      throw handleTransactionError(error);
     }
   }
-  
+
   /**
    * Change the voting/ranking power/order of a particular masternode.
    * @param address The targeted masternode
    * @param capValue The xdc value that will be applied to the targeted address. This value indicates the cap that would like to be increase/reduced on this address
    */
   async changeVote(address: string, capValue: number): Promise<void> {
-    await this.init()
+    await this.init();
     try {
       const { accountAddress } = await this.getGrandMasterAccountDetails();
-      const nonce = await this.web3Client!.eth.getTransactionCount(accountAddress, undefined, { number: FMT_NUMBER.NUMBER , bytes: FMT_BYTES.HEX });
-      
+      const nonce = await this.web3Client!.eth.getTransactionCount(accountAddress, undefined, { number: FMT_NUMBER.NUMBER, bytes: FMT_BYTES.HEX });
+
       if (capValue > 0) {
         await this.web3Contract.methods.vote(replaceXdcWith0x(address)).send({
           from: accountAddress,
@@ -156,10 +159,10 @@ export class GrandMasterManager {
           value: "0x0",
           gas: 220000,
           gasPrice: '250000000'
-        });  
+        });
       }
     } catch (error: any) {
-      throw handleTransactionError(error)
+      throw handleTransactionError(error);
     }
   }
 
@@ -171,9 +174,9 @@ export class GrandMasterManager {
     (this.web3Client!.currentProvider as any).on("accountsChanged", (accounts: string[]) => {
       if (accounts && accounts.length) {
         this.init(true);
-        changeHandler(accounts[0])
+        changeHandler(accounts[0]);
       }
-    })
+    });
   }
 
   /**
@@ -184,7 +187,7 @@ export class GrandMasterManager {
    * 'SLASHED' means it's been taken out from the masternode list
    */
   async getCandidates(): Promise<CandidateDetails[]> {
-    await this.init()
+    await this.init();
     try {
       const result = await this.rpcBasedWeb3!.xdcSubnet.getCandidates("latest");
       if (!result) {
@@ -195,16 +198,16 @@ export class GrandMasterManager {
         throw new ManagerError("Fail to get list of candidates from xdc subnet");
       }
       return Object.entries(candidates).map(entry => {
-        const [ address, { capacity, status }] = entry;
+        const [address, { capacity, status }] = entry;
         return {
           address,
-          delegation: capacity,
+          delegation: weiToEther(capacity),
           status
-        }
+        };
       }).sort((a, b) => b.delegation - a.delegation);
-      
+
     } catch (error: any) {
-      throw handleTransactionError(error)
+      throw handleTransactionError(error);
     }
   }
 }
@@ -217,12 +220,12 @@ const handleTransactionError = (error: any) => {
       case 405:
         return new ManagerError(error.message, ErrorTypes.REVERTED_WITH_NO_REASON);
       default:
-        return new ManagerError("Unable to process this transaction in xdc node, something wrong with your transaction/smart contract call input", ErrorTypes.INVALID_TRANSACTION);    
+        return new ManagerError("Unable to process this transaction in xdc node, something wrong with your transaction/smart contract call input", ErrorTypes.INVALID_TRANSACTION);
     }
   }
   return new ManagerError("Error while processing, but no message found", ErrorTypes.INTERNAL_ERROR);
-}
+};
 
 const replaceXdcWith0x = (address: string) => {
-  return address.replace("xdc", "0x")
-}
+  return address.replace("xdc", "0x");
+};
