1dc8b2d72d1958cb7e8405f9c6bf6882fc15f17c
galaxyscitech <429224280@qq.com>
2023-08-24 12:56:52 +0200

    update

--- frontend/src/pages/management-master-committee-page/components/remove-master-node-dialog/RemoveMasterNodeDialog.tsx
+++ frontend/src/pages/management-master-committee-page/components/remove-master-node-dialog/RemoveMasterNodeDialog.tsx
@@ -1,19 +1,32 @@
-import { useContext, useState } from 'react';
-
-import { DialogButtons, DialogResultBase, DialogTitle } from '@/components/dialog/Dialog';
-import { ServiceContext } from '@/contexts/ServiceContext';
+import { useContext, useState } from "react";
+import WriteButtion from "@/components/WriteButton";
+import Button from "@/components/button/Button";
 import {
-  setMasterNodeDialogFailResult, setMasterNodeDialogSuccessResult
-} from '@/pages/management-master-committee-page/utils/helper';
-import { formatHash } from '@/utils/formatter';
+  DialogButtons,
+  DialogResultBase,
+  DialogTitle,
+} from "@/components/dialog/Dialog";
+import { ServiceContext } from "@/contexts/ServiceContext";
+import {
+  setMasterNodeDialogFailResult,
+  setMasterNodeDialogSuccessResult,
+} from "@/pages/management-master-committee-page/utils/helper";
+import { formatHash } from "@/utils/formatter";
+import { CONTRACT_ADDRESS } from "@/constants/config";
+import ABI from "../../../../abi/ABI.json";
+import { parseEther } from "viem";
 
 interface RemoveMasterNodeDialogProps {
   address: string;
   closeDialog: () => void;
-  setDialogResult?: React.Dispatch<React.SetStateAction<DialogResultBase | undefined>>;
+  setDialogResult?: React.Dispatch<
+    React.SetStateAction<DialogResultBase | undefined>
+  >;
 }
 
-export default function RemoveMasterNodeDialog(props: RemoveMasterNodeDialogProps) {
+export default function RemoveMasterNodeDialog(
+  props: RemoveMasterNodeDialogProps
+) {
   const { address, closeDialog, setDialogResult } = props;
 
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -37,16 +50,55 @@ export default function RemoveMasterNodeDialog(props: RemoveMasterNodeDialogProp
       setIsSubmitting(false);
     }
   }
+  const [rdata, setRdata] = useState({});
+
+  console.log(address);
+
+  (rdata as any)["removeAddress"] = address;
+
+  const removeMaster = {
+    buttonName: "Proceed to wallet confirmation",
+    disabled: !(rdata as any)["removeAddress"],
+    data: {
+      address: CONTRACT_ADDRESS,
+      abi: ABI as any,
+      functionName: "resign",
+      args: [(rdata as any)["removeAddress"]?.replace(/^xdc/, "0x")],
+    },
+  };
 
   return (
     <>
-      <DialogTitle className='text-xl' title={`Are you sure you want to completely remove address ${formatHash(address)} from the master list?`} />
-      <div className='pt-6'>
-        <p>A remove transaction will be generated for your approval. You can add this address back anytime using the
-          <span className='font-bold'> add a new master node</span> function.
+      <DialogTitle
+        className="text-xl"
+        title={`Are you sure you want to completely remove address ${formatHash(
+          address
+        )} from the master list?`}
+      />
+      <div className="pt-6">
+        <p>
+          A remove transaction will be generated for your approval. You can add
+          this address back anytime using the
+          <span className="font-bold"> add a new master node</span> function.
         </p>
       </div>
-      <DialogButtons isSubmitting={isSubmitting} onClose={closeDialog} onSubmit={handleSubmit} submitText='Proceed to wallet confirmation' />
+      <div className="flex gap-2 mt-2">
+        <WriteButtion {...removeMaster} />
+        <Button
+          onClick={closeDialog}
+          className="font-extrabold px-4 py-2.5 flex-1"
+          variant="contained"
+          colour="primary"
+        >
+          {"Cancel"}
+        </Button>
+      </div>
+      {/* <DialogButtons
+        isSubmitting={isSubmitting}
+        onClose={closeDialog}
+        onSubmit={handleSubmit}
+        submitText="Proceed to wallet confirmation"
+      /> */}
     </>
   );
 }
