383290a842a966241ff858adaaf6de3f49b1fa66
galaxyscitech <429224280@qq.com>
2023-08-24 11:17:02 +0200

    update

--- frontend/src/pages/management-master-committee-page/components/promote-dialog/PromoteDialog.tsx
+++ frontend/src/pages/management-master-committee-page/components/promote-dialog/PromoteDialog.tsx
@@ -1,32 +1,39 @@
-import { Form, Formik, FormikContextType, useFormikContext } from 'formik';
-import { useContext, useRef } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import * as Yup from 'yup';
-
-import { DialogButtons, DialogResultBase, DialogTitle } from '@/components/dialog/Dialog';
-import { DialogFormField } from '@/components/form-field/FormField';
-import InfoList from '@/components/info-list/InfoList';
-import { ServiceContext } from '@/contexts/ServiceContext';
+import { Form, Formik, FormikContextType, useFormikContext } from "formik";
+import { useContext, useRef } from "react";
+import { useLoaderData } from "react-router-dom";
+import * as Yup from "yup";
+import WriteButtion from "@/components/WriteButton";
 import {
-  setMasterNodeDialogFailResult, setMasterNodeDialogSuccessResult
-} from '@/pages/management-master-committee-page/utils/helper';
-import { CandidateDetails } from '@/services/grandmaster-manager/statsServiceClient';
-import { formatHash } from '@/utils/formatter';
+  DialogButtons,
+  DialogResultBase,
+  DialogTitle,
+} from "@/components/dialog/Dialog";
+import { DialogFormField } from "@/components/form-field/FormField";
+import InfoList from "@/components/info-list/InfoList";
+import { ServiceContext } from "@/contexts/ServiceContext";
+import {
+  setMasterNodeDialogFailResult,
+  setMasterNodeDialogSuccessResult,
+} from "@/pages/management-master-committee-page/utils/helper";
+import { CandidateDetails } from "@/services/grandmaster-manager/statsServiceClient";
+import { formatHash } from "@/utils/formatter";
 
-import type { ManagementLoaderData } from '@/types/loaderData';
+import type { ManagementLoaderData } from "@/types/loaderData";
 
 interface PromoteDialogProps {
   type: PromoteDialogType;
   data: CandidateDetails;
   closeDialog: () => void;
-  setDialogResult?: React.Dispatch<React.SetStateAction<DialogResultBase | undefined>>;
+  setDialogResult?: React.Dispatch<
+    React.SetStateAction<DialogResultBase | undefined>
+  >;
 }
 
 interface FormValues {
-  increaseDelegation: '' | number;
+  increaseDelegation: "" | number;
 }
 
-type PromoteDialogType = 'promote' | 'demote';
+type PromoteDialogType = "promote" | "demote";
 
 export default function PromoteDialog(props: PromoteDialogProps) {
   const { setDialogResult, data, type } = props;
@@ -37,18 +44,27 @@ export default function PromoteDialog(props: PromoteDialogProps) {
   const service = useContext(ServiceContext);
 
   const initialValues: FormValues = {
-    increaseDelegation: ''
+    increaseDelegation: "",
   };
 
   const validationSchema = Yup.object().shape({
     increaseDelegation: Yup.number()
-      .required('Increase delegation is required')
-      .positive('The value must be greater than 0')
-      .min(type === 'promote' ? minimumDelegation : 0, `The value must be greater than minimum delegation ${minimumDelegation}`),
+      .required("Increase delegation is required")
+      .positive("The value must be greater than 0")
+      .min(
+        type === "promote" ? minimumDelegation : 0,
+        `The value must be greater than minimum delegation ${minimumDelegation}`
+      ),
   });
 
   async function handleSubmit({ increaseDelegation }: FormValues) {
-    if (!increaseDelegation || Number.isNaN(increaseDelegation) || !setDialogResult || !service || !formikRef.current) {
+    if (
+      !increaseDelegation ||
+      Number.isNaN(increaseDelegation) ||
+      !setDialogResult ||
+      !service ||
+      !formikRef.current
+    ) {
       return;
     }
 
@@ -59,7 +75,6 @@ export default function PromoteDialog(props: PromoteDialogProps) {
       setMasterNodeDialogSuccessResult(setDialogResult);
 
       formikRef.current.resetForm();
-
     } catch (error) {
       setMasterNodeDialogFailResult(setDialogResult, error);
     }
@@ -88,31 +103,32 @@ interface UpdatedMasterNodeInfoProps {
 function FormContent(props: PromoteDialogProps) {
   const { type, data, closeDialog } = props;
 
-  const { minimumDelegation, grandmasterRemainingBalance } = useLoaderData() as ManagementLoaderData;
+  const { minimumDelegation, grandmasterRemainingBalance } =
+    useLoaderData() as ManagementLoaderData;
 
   const formik = useFormikContext<FormValues>();
 
   function getTitle(type: PromoteDialogType) {
     switch (type) {
-      case 'promote':
-        return 'Promote via increasing delegation';
-      case 'demote':
-        return 'Demote via decreasing delegation';
+      case "promote":
+        return "Promote via increasing delegation";
+      case "demote":
+        return "Demote via decreasing delegation";
 
       default:
-        return '';
+        return "";
     }
   }
 
   function getActionLabel(type: PromoteDialogType) {
     switch (type) {
-      case 'promote':
-        return 'Increase delegation by:';
-      case 'demote':
-        return 'Decrease delegation by:';
+      case "promote":
+        return "Increase delegation by:";
+      case "demote":
+        return "Decrease delegation by:";
 
       default:
-        return '';
+        return "";
     }
   }
 
@@ -124,10 +140,19 @@ function FormContent(props: PromoteDialogProps) {
 
   const initInfo = {
     data: [
-      { name: 'Grandmaster\'s remaining balance:', value: `${grandmasterRemainingBalance} xdc` },
-      { name: `${formattedAddress}'s current delegation:`, value: `${delegation} hxdc` },
-      { name: `${formattedAddress}'s minimum delegation:`, value: `${minimumDelegation} hxdc` },
-    ]
+      {
+        name: "Grandmaster's remaining balance:",
+        value: `${grandmasterRemainingBalance} xdc`,
+      },
+      {
+        name: `${formattedAddress}'s current delegation:`,
+        value: `${delegation} hxdc`,
+      },
+      {
+        name: `${formattedAddress}'s minimum delegation:`,
+        value: `${minimumDelegation} hxdc`,
+      },
+    ],
   };
 
   return (
@@ -135,30 +160,49 @@ function FormContent(props: PromoteDialogProps) {
       <DialogTitle title={title} />
 
       {/* Dialog content */}
-      <div className='pt-6'>
-        <InfoList info={initInfo} noIcon valueClassName='text-lg' />
+      <div className="pt-6">
+        <InfoList info={initInfo} noIcon valueClassName="text-lg" />
       </div>
       <DialogFormField
         labelText={actionLabel}
-        name='increaseDelegation'
-        type='number'
-        valueSuffix='xdc'
-        className='pt-6'
-        isError={!!(formik.errors.increaseDelegation && formik.touched.increaseDelegation)}
+        name="increaseDelegation"
+        type="number"
+        valueSuffix="xdc"
+        className="pt-6"
+        isError={
+          !!(
+            formik.errors.increaseDelegation &&
+            formik.touched.increaseDelegation
+          )
+        }
       />
-      <PreviewUpdatedMasterNodeInfo delegation={delegation} formattedAddress={formattedAddress} type={type} />
+      <PreviewUpdatedMasterNodeInfo
+        delegation={delegation}
+        formattedAddress={formattedAddress}
+        type={type}
+      />
 
-      <DialogButtons isSubmitting={formik.isSubmitting} omitSeparator submitText='Proceed to wallet confirmation' onClose={closeDialog} />
+      <WriteButtion />
+      <DialogButtons
+        isSubmitting={formik.isSubmitting}
+        omitSeparator
+        submitText="Proceed to wallet confirmation"
+        onClose={closeDialog}
+      />
     </>
   );
 }
 
-function getUpdatedDelegation(delegation: number, increaseDelegation: number | '', type: PromoteDialogType) {
+function getUpdatedDelegation(
+  delegation: number,
+  increaseDelegation: number | "",
+  type: PromoteDialogType
+) {
   if (!increaseDelegation || Number.isNaN(increaseDelegation)) {
     return delegation;
   }
 
-  if (type === 'demote') {
+  if (type === "demote") {
     return delegation - increaseDelegation;
   }
 
@@ -166,26 +210,37 @@ function getUpdatedDelegation(delegation: number, increaseDelegation: number | '
 }
 
 function getDelegation(increaseDelegation: number, type: PromoteDialogType) {
-  if (type === 'demote') {
+  if (type === "demote") {
     return -increaseDelegation;
   }
 
   return increaseDelegation;
 }
 
-function PreviewUpdatedMasterNodeInfo({ delegation, formattedAddress, type }: UpdatedMasterNodeInfoProps) {
+function PreviewUpdatedMasterNodeInfo({
+  delegation,
+  formattedAddress,
+  type,
+}: UpdatedMasterNodeInfoProps) {
   const formikContext = useFormikContext<FormValues>();
-  const updatedDelegation = getUpdatedDelegation(delegation, formikContext.values.increaseDelegation, type);
+  const updatedDelegation = getUpdatedDelegation(
+    delegation,
+    formikContext.values.increaseDelegation,
+    type
+  );
 
   const changedInfo = {
     data: [
-      { name: `${formattedAddress}'s new delegation:`, value: `${updatedDelegation} hxdc` },
-    ]
+      {
+        name: `${formattedAddress}'s new delegation:`,
+        value: `${updatedDelegation} hxdc`,
+      },
+    ],
   };
 
   return (
-    <div className='pt-6'>
-      <InfoList info={changedInfo} noIcon valueClassName='text-lg' />
+    <div className="pt-6">
+      <InfoList info={changedInfo} noIcon valueClassName="text-lg" />
     </div>
   );
 }
