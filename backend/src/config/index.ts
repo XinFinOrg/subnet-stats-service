29646ac60e987124497b73b7d17e07331cc180ef
wanwiset25 <wanwiset25@gmail.com>
2023-11-29 17:09:29 +0400

    change env name for consistency across all services

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -5,15 +5,15 @@ import { logger } from '../utils/logger';
 export const MAX_NUM_OF_BLOCKS_IN_HISTORY = 300;
 export const NUM_OF_BLOCKS_RETURN = 50;
 
-export const { SUBNET_URL, PARENTCHAIN_URL } = process.env;
-if (!SUBNET_URL || !PARENTCHAIN_URL) {
-  logger.error('SUBNET_URL and PARENTCHAIN_URL configuration must be set!');
+export const { SUBNET_URL, PARENTNET_URL } = process.env;
+if (!SUBNET_URL || !PARENTNET_URL) {
+  logger.error('SUBNET_URL and PARENTNET_URL configuration must be set!');
   process.exit(1);
 }
 
-export const { PARENTCHAIN_WALLET, CHECKPOINT_CONTRACT } = process.env;
-if (!PARENTCHAIN_WALLET || !CHECKPOINT_CONTRACT) {
-  logger.error('PARENTCHAIN_WALLET and CHECKPOINT_CONTRACT configuration must be set!');
+export const { PARENTNET_WALLET, CHECKPOINT_CONTRACT } = process.env;
+if (!PARENTNET_WALLET || !CHECKPOINT_CONTRACT) {
+  logger.error('PARENTNET_WALLET and CHECKPOINT_CONTRACT configuration must be set!');
   process.exit(1);
 }
 
